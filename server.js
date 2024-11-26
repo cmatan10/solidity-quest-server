063ec667ba1e971420cd41a8f95d9604d8dfe585
cmatan10 <cmatan10@gmail.com>
2024-11-27 01:49:23 +0200

    modify to vercel

--- server.js
+++ server.js
@@ -18,8 +18,11 @@ app.get('/', (req, res) => {
   res.send('Solidity Quest');
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// Remove the app.listen() call
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 
+// Export the app for Vercel to use
+module.exports = app;
