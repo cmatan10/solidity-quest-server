7d092741c7f1c184c141bbc16a8bfdfbceaaf8af
cmatan10 <cmatan10@gmail.com>
2024-11-27 01:31:01 +0200

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
   res.send('Solidity Quest');
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
