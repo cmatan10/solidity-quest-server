32c61ab26b85d21d20fbc0f3ac5ca9b806672889
cmatan10 <cmatan10@gmail.com>
2024-12-06 16:30:45 +0200

    private data contract

---
+++ Hardhat/contracts/CodeEditorGames/PrivateData.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract SecretData {
+    
+    bytes32 public hexadecimal = 0x48656c6c6f210000000000000000000000000000000000000000000000000000;
+
+    address public creator;
+    uint96 public number = 1000;
+
+    uint256 public bigNumber = 1000000000;
+
+    string public str = "I am public";
+
+    string[] private array;
+
+    bool public secretRevealed;
+
+    constructor(string memory _itsNotThePassword, string memory _itsThePassword){
+        creator = msg.sender;
+        array.push(_itsNotThePassword);
+        array.push(_itsThePassword);
+    }
+
+    function Reveale(string memory _secret) public {
+        require(keccak256(abi.encodePacked(_secret)) == keccak256(abi.encodePacked(array[1])), "Secret does not match");
+        secretRevealed = true;
+    }
+}
+
